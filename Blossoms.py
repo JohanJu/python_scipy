e26f62c6a907bdedee44f9898a659da6031ed99c
Johan Ju <johanj93@gmail.com>
2015-09-19 11:51:46 +0200

    other version

--- Blossoms.py
+++ Blossoms.py
@@ -42,3 +42,41 @@ basisF3 = basisFunction3(3, array([0.,1,2,3,4,5,6,7,8,9,10]))
  
 plotFunction(basisF,0,9)
 plotFunction(basisF3,0,9)
+
+"""
+annan version
+"""
+
+K = 10;
+ui = list(range(K))
+ui.append(-2)
+ui.append(-1)
+u = array(linspace(0,K-2,1000))
+print(ui)
+
+def N0(i,j):
+    re = []
+    for j in u:
+        if(ui[i-1]<=j and j<ui[i]):
+            re.append(1)
+        else:
+            re.append(0)
+    return re
+
+def Nk(k,i,u):
+    if(k>1):
+        ret=(u-ui[i-1])*Nk(k-1,i,u)/(ui[i+k-1]-ui[i-1])
+        ret+=(ui[i+k]-u)*Nk(k-1,i+1,u)/(ui[i+k]-ui[i])
+    else:
+        ret=(u-ui[i-1])*N0(i,u)/(ui[i+k-1]-ui[i-1])
+        ret+=(ui[i+k]-u)*N0(i+1,u)/(ui[i+k]-ui[i])
+    return ret
+    
+
+plot(u,Nk(3,2,u))
+
+y = array(zeros(1000))
+for i in range(-2,K-1):
+    y+=Nk(3,i,u)
+    
+plot(u,y)
