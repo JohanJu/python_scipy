4e36e186ae05e0738fd81554ca3a9ac8eb7092b9
Johan Ju <johanj93@gmail.com>
2015-09-21 21:15:54 +0200

    BlossomSpline Class

--- Blossoms.py
+++ Blossoms.py
@@ -78,39 +78,31 @@ mui= array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 dx = array([0,0,0,1,2,3,5,8,10,10,8,6,3,0,0,0])
 dy = array([0,0,0,1,4,6,8,0,10,8,4,2,2,0,0,0])
 s=Spline(mui,dx,dy)
-s.plot(1,1)
+s.plot(0,1)
 
-ui= array(range(16))
-d = array([0,0,0,1,4,6,8,0,10,8,4,2,2,0,0,0])
 
-def Alpha(u, rm, lm):
-    return (rm-u)/(rm-lm)
-
-def Blossom(u, I):
-#    d11 = Alpha(u,ui[I+1],ui[I-2])*d[I-2]+(1-Alpha(u,ui[I+1], ui[I-2]))*d[I-1]
-#    d12 = Alpha(u, ui[I+2], ui[I-1])*d[I-1]+(1-Alpha(u,ui[I+2], ui[I-1]))*d[I]
-#    d13 = Alpha(u, ui[I+3], ui[I])*d[I]+(1-Alpha(u,ui[I+3], ui[I]))*d[I+1]
-#    
-#    d21 = Alpha(u, ui[I+2], ui[I-2])*d11+(1-Alpha(u,ui[I+2], ui[I-2]))*d12
-#    d22 = Alpha(u, ui[I+3], ui[I-1])*d12+(1-Alpha(u,ui[I+3], ui[I-1]))*d13
-#    return Alpha(u, ui[I+3], ui[I-2])*d21+(1-Alpha(u,ui[I+3], ui[I-2]))*d22
-    d11 = Alpha(u,ui[I+1],ui[I-2])*d[I-2]+(1-Alpha(u,ui[I+1], ui[I-2]))*d[I-1]
-    d12 = Alpha(u, ui[I+2], ui[I-1])*d[I-1]+(1-Alpha(u,ui[I+2], ui[I-1]))*d[I]
-    d13 = Alpha(u, ui[I+3], ui[I])*d[I]+(1-Alpha(u,ui[I+3], ui[I]))*d[I+1]
+class BlossomSpline():
     
-    d21 = Alpha(u, ui[I+1], ui[I-1])*d11+(1-Alpha(u,ui[I+1], ui[I-1]))*d12
-    d22 = Alpha(u, ui[I+2], ui[I])*d12+(1-Alpha(u,ui[I+2], ui[I]))*d13
-    
-    return Alpha(u, ui[I+1], ui[I])*d21+(1-Alpha(u,ui[I+1], ui[I]))*d22 
-    
-       
-
-y=[]
-for i in range(2,12):
-    y.append(Blossom(i, i))
-
-plot(range(2,12),y,'o')
-
-#basisF3 = s.basisFunction3(3, array([0.,1,2,3,4,5,6,7,8,9,10]))
-#print(basisF3(4))
-#s.plotFunction(basisF3,0,9)
+    def __call__(self,u):
+        I = (self.ui>=u).argmax()-1
+        d11 = self.alpha(u,self.ui[I+1],self.ui[I-2])*self.d[I-2]+(1-self.alpha(u,self.ui[I+1], self.ui[I-2]))*self.d[I-1]
+        d12 = self.alpha(u, self.ui[I+2], self.ui[I-1])*self.d[I-1]+(1-self.alpha(u,self.ui[I+2], self.ui[I-1]))*self.d[I]
+        d13 = self.alpha(u, self.ui[I+3], self.ui[I])*self.d[I]+(1-self.alpha(u,self.ui[I+3], self.ui[I]))*self.d[I+1]
+        d21 = self.alpha(u, self.ui[I+1], self.ui[I-1])*d11+(1-self.alpha(u,self.ui[I+1], self.ui[I-1]))*d12
+        d22 = self.alpha(u, self.ui[I+2], self.ui[I])*d12+(1-self.alpha(u,self.ui[I+2], self.ui[I]))*d13
+        return self.alpha(u, self.ui[I+1], self.ui[I])*d21+(1-self.alpha(u,self.ui[I+1], self.ui[I]))*d22 
+    def __init__(self,ui,dx,dy):
+        self.ui=ui
+        self.dx=dx
+        self.d=dy
+    def alpha(self,u, rm, lm):
+        return (rm-u)/(rm-lm)
+    def plot(self,start,end,nbr):
+        lx = linspace(start,end,nbr)
+        ly = []
+        for i in lx:
+            ly.append(self(i))
+        plot(lx,ly,'o')
+        
+bs=BlossomSpline(mui,dx,dy)
+bs.plot(2,13,30)
