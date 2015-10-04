acbfdf84602de3bf8b03ff993a0a56ac08acffda
cbrandberg@me.com <cbrandberg@me.com>
2015-10-04 19:19:03 +0200

    added: inexact line search, DFP rank-2 update, BFGS rank-2 update. None of them tested.

---
+++ python_scipy/optimization/Generic.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from scipy import *
+                                                #hur äe denna generisk? 
+class QuasiNewton:
+    def __init__(self,problem,H,a):
+        self.problem = problem
+        self.H = H
+        self.a = a
+        
+    def slove(self,x,tolerance):
+        error = 1000;
+        h = eye(len(x))
+        while(error>tolerance):
+            s=-dot(h,self.problem.grad(x))
+            xn = x+self.a(self.problem(),x,s)*x     #xn = x + a*s ? 
+            p = self.problem()
+            error = abs(p(xn)-p(x))
+            delta = xn-x
+            gamma = self.problem.grad(xn)-self.problem.grad(x)
+            x = xn
+            h=self.H(h,delta,gamma)
+        return x
